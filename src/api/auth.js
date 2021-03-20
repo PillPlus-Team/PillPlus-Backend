a285212ca395b223f865196081f5f8f42e59f56c
Poomipat-Ch <poomipat002@gmail.com>
2021-03-21 02:09:15 +0700

    ✔✔✔

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,81 +1,78 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const expressJwt = require('express-jwt')
-const jwt = require('jsonwebtoken')
-const { notFound } = require('../common/middleware')
-const { errorRes, successRes, errData } = require('../common/response')
-const bcrypt = require('bcrypt')
-const User = require('../models/User')
-const router  = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
+const expressJwt = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const { notFound } = require("../common/middleware");
+const { errorRes, successRes, errData } = require("../common/response");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const router = express.Router();
 
+router.use(isValidPassword);
+router.post("/signup", hasPassword, signUp);
+router.post("/login", findByEmail, verifyPassword, login);
 
-router.use(isValidPassword)
-router.post('/signup', hasPassword, signUp)
-router.post('/login', findByEmail, verifyPassword, login)
+router.use(notFound);
 
-router.use(notFound)
-
-
-function isValidPassword (req, res, next) {
-    const { password } = req.body
-    if (!password || password.length < 6) {
-        const err = `invalid password ${password}`
-        const errMsg = 'password must be at least 6 characters'
-        return errorRes(res, err, errMsg)
-    }
-    return next()
+function isValidPassword(req, res, next) {
+  const { password } = req.body;
+  if (!password || password.length < 6) {
+    const err = `invalid password ${password}`;
+    const errMsg = "password must be at least 6 characters";
+    return errorRes(res, err, errMsg);
+  }
+  return next();
 }
 
-function findByEmail (req, res, next) {
-    const { email, password } = req.body
-    User.findOne({ email }, '+password', { lean: true }, (err, data) => {
-        if (err || !data)
-            return errorRes(res, 'invalid login', 'invalid password or email')
-        req.body = { unhashedPassword: password, ...data }
-        return next()
-    } )
+function findByEmail(req, res, next) {
+  const { email, password } = req.body;
+  User.findOne({ email }, "+password", { lean: true }, (err, data) => {
+    if (err || !data)
+      return errorRes(res, "invalid login", "invalid password or email");
+    req.body = { unhashedPassword: password, ...data };
+    return next();
+  });
 }
 
-function signUp (req, res) {
-    const newUser = new User({
-        _id: new mongoose.Types.ObjectId(),
-        ...req.body
-    })
-    return newUser.save((err, data) => {
-        if (err)
-            return errorRes(res, err, 'unable to create user')
-        
-        const { _id, name, email, type } = data
-        return successRes(res, { _id, name, email, type })
-    })
+function signUp(req, res) {
+  const newUser = new User({
+    _id: new mongoose.Types.ObjectId(),
+    ...req.body,
+  });
+  return newUser.save((err, data) => {
+    if (err) return errorRes(res, err, "unable to create user");
+
+    const { _id, name, email, type } = data;
+    return successRes(res, { _id, name, email, type });
+  });
 }
 
-function hasPassword (req, res, next) {
-    const { password } = req.body
-    bcrypt.hash(password, 10, (err, hashed) => {
-        if (err)
-            return errorRes(res, err, 'unable to sign up, try again')
-        req.body.password = hashed
-        return next()
-    })
+function hasPassword(req, res, next) {
+  const { password } = req.body;
+  bcrypt.hash(password, 10, (err, hashed) => {
+    if (err) return errorRes(res, err, "unable to sign up, try again");
+    req.body.password = hashed;
+    return next();
+  });
 }
 
-function verifyPassword (req, res, next){
-    const { unhashedPassword, password, ...userData } = req.body
-    bcrypt.compare(unhashedPassword, password, (err, same) => {
-        if (same) {
-            req.body = userData
-            return next()
-        }else 
-            return errorRes(res, err, 'password error, try again')
-    })
+function verifyPassword(req, res, next) {
+  const { unhashedPassword, password, ...userData } = req.body;
+  bcrypt.compare(unhashedPassword, password, (err, same) => {
+    if (same) {
+      req.body = userData;
+      return next();
+    } else return errorRes(res, err, "password error, try again");
+  });
 }
 
-function login (req, res) {
-    jwt.sign(req.body, process.env.JWT_SECRET,
-        {algorithm: 'HS512', expiresIn: '31d'}, 
-        errData(res, 'token error')
-    )
+function login(req, res) {
+  jwt.sign(
+    req.body,
+    process.env.JWT_SECRET,
+    { algorithm: "HS512", expiresIn: "1d" },
+    errData(res, "token error")
+  );
 }
 
-module.exports = router
+module.exports = router;
--- src/api/user.js
+++ src/api/user.js
@@ -1,41 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { create, read, update, remove } = require('../common/crud')
-const User = require('../models/User');
-const { onlyAdmin, notFound } = require('../common/middleware');
-const bcrypt = require('bcrypt');
-const { errorRes } = require('../common/response');
+const { create, read, update, remove } = require("../common/crud");
+const User = require("../models/User");
+const { onlyAdmin, notFound } = require("../common/middleware");
+const bcrypt = require("bcrypt");
+const { errorRes } = require("../common/response");
 
-router.use(onlyAdmin)
-router.post('/', create(User))
-router.get('/all/:page', userAtPage, read(User))
-router.put('/:_id', handlePassword, update(User))
-router.delete('/:_id', remove(User))
+router.use(onlyAdmin);
+router.post("/", create(User));
+router.get("/all/:page", userAtPage, read(User));
+router.put("/:_id", handlePassword, update(User));
+router.delete("/:_id", remove(User));
 
-router.use(notFound)
+router.use(notFound);
 
-function userAtPage (req, res, next) {
-    req.body = [ {}, null, { limit: 25, skip: (req.params.page-1) * 25 } ]
-    return next()
+function userAtPage(req, res, next) {
+  req.body = [{}, null, { limit: 25, skip: (req.params.page - 1) * 25 }];
+  return next();
 }
 
-function handlePassword (req, res, next) {
-    const { password, ...body } = req.body
-    if (!password || password.length < 1){
-        req.body = body
-        return next()
-    }
-    if (password.length < 6)
-        return errorRes(res, 'invalid password', 'password must be at least 6 characters')
-    
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err)
-            return errorRes(res, err, 'password error')
-        const data = {...body, password: hash}
-        req.body = data
-        return next()
-    })
+function handlePassword(req, res, next) {
+  const { password, ...body } = req.body;
+  if (!password || password.length < 1) {
+    req.body = body;
+    return next();
+  }
+  if (password.length < 6)
+    return errorRes(
+      res,
+      "invalid password",
+      "password must be at least 6 characters"
+    );
+
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) return errorRes(res, err, "password error");
+    const data = { ...body, password: hash };
+    req.body = data;
+    return next();
+  });
 }
 
 module.exports = router;
-
