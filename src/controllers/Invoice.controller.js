39ac3f833fd2afcf18e25e2871b1dfb0f5fb815d
Poomipat-Ch <poomipat002@gmail.com>
2021-05-03 16:34:03 +0700

    fix statement

--- src/controllers/Invoice.controller.js
+++ src/controllers/Invoice.controller.js
@@ -280,8 +280,8 @@ exports.getStatements = (req, res) => {
   Invoice.find({
     "pillStore._id": req.user._id,
     created_at: {
-      $gte: new Timestamp(new Date(req.body.year, req.body.month, 1), 0),
-      $lt: new Timestamp(new Date(req.body.year, req.body.month + 1, 1), 0),
+      $gte: ISODate(`${req.body.year}-${req.body.month}-1`),
+      $lt: ISODate(`${req.body.year}-${req.body.month + 1}-1`),
     },
   })
     .populate("pillStore")
