17327e38e0c5816969c41dc80230f69bfc24a361
Poomipat-Ch <poomipat002@gmail.com>
2021-05-03 15:45:57 +0700

    add api statement for Pill store

--- src/api/invoice.js
+++ src/api/invoice.js
@@ -5,22 +5,28 @@ const {
   forStaff,
   forCashier,
   forPatient,
-  forPillStore
+  forPillStore,
 } = require("../common/middleware");
 
-const controller = require('../controllers/Invoice.controller');
+const controller = require("../controllers/Invoice.controller");
 
 // ---------------------------- API ---------------------------- //
 
 // ----------------------- For Pill Store ---------------------- //
 router.get("/listCustomers", forPillStore, controller.getListCustomers);
 router.put("/dispensePill/:_id", forPillStore, controller.dispensePill);
+router.get("/statement", forPillStore, controller.getStatement);
 
 // ----------------------- For Patients ------------------------ //
 router.put("/update", forPatient, controller.updateInvoice);
 
 // ------------------------ For Hospital ----------------------- //
-router.post("/selectPillStore", forStaff, controller.createQueue, controller.selectPillStore);
+router.post(
+  "/selectPillStore",
+  forStaff,
+  controller.createQueue,
+  controller.selectPillStore
+);
 
 router.get("/", forCashier, controller.getAllInvoices); // fetch All data
 router.post("/:_id", forCashier, controller.invoicePaid);
